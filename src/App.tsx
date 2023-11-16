19138a01ab7e245df4c3c246496e265149d7dcbf
nelsonvilte <nelsonvilte@gmail.com>
2023-11-16 17:08:49 -0300

    agregados componentes categorias y footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar'
 import CarouselHome from './components/CarouselHome/CarouselHome'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <>
         <NavBar/>
         <CarouselHome/>
+        <Footer/>
     </>
   )
 }
