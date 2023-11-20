df7995a0bc92bf876f21ee0b448e80bd8c8a73f1
nelsonvilte <nelsonvilte@gmail.com>
2023-11-20 19:09:26 -0300

    agregados componentes Categoria y creación de servicio para comunicación con API

--- src/components/Categoria/Categoria.tsx
+++ src/components/Categoria/Categoria.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react"
+import { Task } from "../../types/Task"
+import CategoriaSelector from "../CategoriaSelector/CategoriaSelector";
+import CategoriaTareas from "../CategoriaTareas/CategoriaTareas";
+import { TaskService } from "../../Services/TaskServices";
 
 const Categoria = () => {
+const [tasks, setTasks]=useState<Task[]>([]);
+const [selectedCategory, setSelectedCategory]= useState<string>('');//estado para la categoria seleccionada
+
+useEffect(()=>{
+  const fetchTasks = async()=>{
+    const taskData = await TaskService.getAllTasks();
+    setTasks(taskData);
+  };
+  fetchTasks();
+}, []);
+
+//filtra las tareas por la categoria seleccionada
+const filteredTasks = selectedCategory
+? tasks.filter(task=> task.estado.toUpperCase()=== selectedCategory.toLocaleUpperCase())
+: tasks;
+
+
   return (
-    <div>Categoria</div>
+    <div className="container mt-5">
+       <CategoriaSelector onSelectedCategogry={setSelectedCategory}/>{/* Pasa la funcion para manejar la seleccion de categoria */}
+       <CategoriaTareas tasks={filteredTasks}/>{/* Pasa las tareas filtradas al componente CategoriasTareas */}
+
+    </div>
   )
 }
 
--- src/components/CategoriaSelector/CategoriaSelector.tsx
+++ src/components/CategoriaSelector/CategoriaSelector.tsx
@@ -1,7 +1,33 @@
+import {BsBookmarkCheck, BsCheck, BsGear, BsPencilSquare} from 'react-icons/bs'
 
-const CategoriaSelector = () => {
+interface CategoriasSelectorProps{
+  onSelectedCategogry: (catergoria:string)=>void;
+}
+const CategoriaSelector: React.FC<CategoriasSelectorProps> = ({onSelectedCategogry}) => {
+const categorias=[
+  {nombre:'PORHACER', icono:<BsCheck/>},
+  {nombre:'ENPRODUCCION', icono:<BsGear/>},
+  {nombre:'PORTESTEAR', icono:<BsPencilSquare/>},
+  {nombre:'COMPLETADA', icono:<BsBookmarkCheck/>},    
+];
+
   return (
-    <div>CategoriaSelector</div>
+   <section className="container mt-3" id="selector-categorias">
+   <p className="fs-3">Seleccione una categoria</p>
+   <div className="row gap-4">
+    {categorias.map((categoria,index)=>(
+      <div className="col d-flex justify-content-center p-0" key={index}>
+        <button
+          onClick={()=> onSelectedCategogry(categoria.nombre)}
+          className="border border-1 boreder-black d-flex gap-1 align-items-center rounded p-1 text-decoration-none"
+          style={{cursor:'pointer'}}
+        >{categoria.icono} {categoria.nombre}</button>
+
+      </div>
+    ))}
+   </div>
+
+   </section>
   )
 }
 
