4070c194ba602340abaa348fc75f6ec8ff501093
nelsonvilte <nelsonvilte@gmail.com>
2023-11-22 18:15:28 -0300

    actualizacion url base

--- src/Services/TaskServices.ts
+++ src/Services/TaskServices.ts
@@ -1,6 +1,6 @@
 import { Task } from "../types/Task";
 
-const BASE_URL ="http://localhost:3000/tasks"; 
+const BASE_URL ="https://backend-taskapp-oftb.onrender.com/tasks"; 
 
 export const TaskService ={
     
