1c45c588a891084a0793467d1022558b28f4e9a7
nelsonvilte <nelsonvilte@gmail.com>
2023-11-23 02:23:23 -0300

    corrección configuración componente ToastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Footer from "./components/Footer/Footer"
 import NavBar from "./components/NavBar/NavBar"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import AppRoutes from "./routes/AppRoutes"
 import {BrowserRouter as Router} from "react-router-dom"
 
 //Notificaciones al usuario
-import { ToastContainer } from "react-bootstrap"
+import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
 const App = () => {
