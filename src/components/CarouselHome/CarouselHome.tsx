4f52e22ca6a737f0518f009d75cce5b752e62c1c
nelsonvilte <nelsonvilte@gmail.com>
2023-11-24 18:58:10 -0300

    actualizacion index

--- src/components/CarouselHome/CarouselHome.tsx
+++ src/components/CarouselHome/CarouselHome.tsx
@@ -17,7 +17,7 @@ const CarouselHome = () => {
         </Carousel.Caption>
       </Carousel.Item>
 
-      <Carousel.Item>S
+      <Carousel.Item>
       <img 
         className='d block w-100'
         style={{maxHeight:"500px", objectFit:'cover'}}
